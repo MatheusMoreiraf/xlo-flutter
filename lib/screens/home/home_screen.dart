dfe4e94c7e7289096f7bbd33fe20e1e5b4e60853
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2020-03-31 15:08:43 -0300

    Creating the Product Screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -90,6 +90,8 @@ class _HomeScreenState extends State<HomeScreen> {
               stream: _homeBloc.outAd,
               initialData: [],
               builder: (context, snapshot) {
+                if(snapshot.data == null)
+                  return Container();
                 return ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder: (context, index) {
